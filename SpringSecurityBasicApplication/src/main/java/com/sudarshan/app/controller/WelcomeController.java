853c295a575fbdf52cc7f78dad023635837a93fd
Sudarshan Pradhan <81807230+sudarshan-pradhan@users.noreply.github.com>
2022-06-04 07:07:35 +0530

    A basic spring security app with user and password

---
+++ SpringSecurityBasicApplication/src/main/java/com/sudarshan/app/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.sudarshan.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to spring boot application with spring security!";
+	}
+}
