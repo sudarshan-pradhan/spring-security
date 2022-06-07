220d11b39afba374001d661195c815dfcc835963
Sudarshan Pradhan <81807230+sudarshan-pradhan@users.noreply.github.com>
2022-06-08 00:09:41 +0530

    Approach-2 in-memory users

--- SpringSecurityBasicApplication-2/src/main/java/com/sudarshan/app/configuration/AppSecurityConfiguration.java
+++ SpringSecurityBasicApplication-2/src/main/java/com/sudarshan/app/configuration/AppSecurityConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -27,27 +29,60 @@ public class AppSecurityConfiguration {
 		return http.build();
 
 	}
+//	
+//	/**
+//	 * Approach-1
+//	 * 
+//	 * method for in-memory users for spring security
+//	 * users and their roles have to be defined in the same method
+//	 * 
+//	 * @return
+//	 */
+//	@Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//    	UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("12345")
+//                .authorities("admin")
+//                .build();
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("12345")
+//                .authorities("read")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
 	
 	/**
-	 * Approach-1
+	 * Approach-2
 	 * 
-	 * method for in-memory users for spring security
-	 * users and their roles have to be defined in the same method
+	 * where we don't define password encoder
+	 * while creating the user details. Instead a separate
+	 * PasswordEncoder bean will be created.
 	 * 
 	 * @return
 	 */
 	@Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-    	UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("12345")
-                .authorities("read")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
+	public InMemoryUserDetailsManager userDetailsService() {
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+        UserDetails admin = User.withUsername("admin").password("12345").authorities("admin").build();
+        UserDetails user = User.withUsername("user").password("12345").authorities("read").build();
+        userDetailsService.createUser(admin);
+        userDetailsService.createUser(user);
+        return userDetailsService;
+	}
+	
+    /**
+     * Approach-2
+     * 
+     * NoOpPasswordEncoder is not recommended for production usage.
+     * Use only for non-prod.
+     *
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
+	
 }
